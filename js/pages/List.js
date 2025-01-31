790e5da7f548e4f613c1dfd10c81306c0a224bac
alcra <65140768+alcragd@users.noreply.github.com>
2025-01-30 22:31:09 -0600

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -22,32 +22,90 @@ export default {
         </main>
         <main v-else class="page-list">
             <div class="list-container">
-                <select v-model="selectedPage" @change="updateView">
-                    <option value="list">List</option>
-                    <option value="leaderboard">Leaderboard</option>
-                    <option value="info">Information</option>
-                </select>
-                <div v-if="selectedPage === 'list'">
-                    <table class="list" v-if="list">
-                        <tr v-for="([level, err], i) in list">
-                            <td class="rank">
-                                <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
-                                <p v-else class="type-label-lg">Legacy</p>
-                            </td>
-                            <td class="level" :class="{ 'active': selected == i, 'error': !level }">
-                                <button @click="selected = i">
-                                    <span class="type-label-lg">{{ level?.name || ('Error (' + err + '.json)') }}</span>
-                                </button>
-                            </td>
-                        </tr>
+                <table class="list" v-if="list">
+                    <tr v-for="([level, err], i) in list">
+                        <td class="rank">
+                            <p v-if="i + 1 <= 150" class="type-label-lg">#{{ i + 1 }}</p>
+                            <p v-else class="type-label-lg">Legacy</p>
+                        </td>
+                        <td class="level" :class="{ 'active': selected == i, 'error': !level }">
+                            <button @click="selected = i">
+                                <span class="type-label-lg">{{ level?.name || \`Error (\${err}.json)\` }}</span>
+                            </button>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+            <div class="level-container">
+                <div class="level" v-if="level">
+                    <h1>{{ level.name }}</h1>
+                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="Array.isArray(level.verifier) ? level.verifier.join(', ') : level.verifier"></LevelAuthors>
+                    <div v-for="vid in video" :key="vid">
+                        <iframe :src="vid" frameborder="0"  width="552" height="320" allowfullscreen></iframe>
+                    </div>
+                    <ul class="stats">
+                        <li>
+                            <div class="type-title-sm">Points when completed</div>
+                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">ID</div>
+                            <p>{{ level.id }}</p>
+                        </li>
+                        <li>
+                            <div class="type-title-sm">Password</div>
+                            <p>{{ level.password || 'Free to Copy' }}</p>
+                        </li>
+                    </ul>
+                    <h2>Records</h2>
+                    <p v-if="selected + 1 <= 75"><strong>{{ level.percentToQualify }}%</strong> or better to qualify</p>
+                    <p v-else-if="selected +1 <= 150"><strong>100%</strong> or better to qualify</p>
+                    <p v-else>This level does not accept new records.</p>
+                    <table class="records">
+<tr v-for="record in level.records" class="record">
+    <td class="percent">
+        <p>{{ record.percent }}%</p>
+    </td>
+    <td class="user">
+    <div v-for="(user, index) in record.user" :key="index">
+        <a :href="record.link[index]" target="_blank" class="type-label-lg">
+            {{ user }}
+        </a>
+    </div>
+</td>
+    <td class="mobile">
+<img v-if="record.mobile" :src="'/assets/phone-landscape' + (store.dark ? '-dark' : '') + '.svg'" alt="Mobile">
+    </td>
+    <td class="hz">
+        <p>{{ record.hz }}Hz</p>
+    </td>
+</tr>
+
                     </table>
                 </div>
-                <div v-if="selectedPage === 'leaderboard'">
-                    <h2>Leaderboard</h2>
-                    <p>Leaderboard data will be displayed here.</p>
+                <div v-else class="level" style="height: 100%; justify-content: center; align-items: center;">
+                    <p>(ノಠ益ಠ)ノ彡┻━┻</p>
                 </div>
-                <div v-if="selectedPage === 'info'">
-                    <h2>Submission Requirements</h2>
+            </div>
+            <div class="meta-container">
+                <div class="meta">
+                    <div class="errors" v-show="errors.length > 0">
+                        <p class="error" v-for="error of errors">{{ error }}</p>
+                    </div>
+                    <div class="og">
+                        <p class="type-label-md">Website layout made by <a href="https://tsl.pages.dev/" target="_blank">TheShittyList</a></p>
+                    </div>
+                    <template v-if="editors">
+                        <h3>List Editors</h3>
+                        <ol class="editors">
+                            <li v-for="editor in editors">
+                                <img :src="\`/assets/\${roleIconMap[editor.role]}\${store.dark ? '-dark' : ''}.svg\`" :alt="editor.role">
+                                <a v-if="editor.link" class="type-label-lg link" target="_blank" :href="editor.link">{{ editor.name }}</a>
+                                <p v-else>{{ editor.name }}</p>
+                            </li>
+                        </ol>
+                    </template>
+                    <h3>Submission Requirements</h3>
                     <p>
                         Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
                     </p>
@@ -74,34 +132,11 @@ export default {
                     </p>
                 </div>
             </div>
-            <div v-if="selectedPage === 'list' && level" class="level-container">
-                <div class="level">
-                    <h1>{{ level.name }}</h1>
-                    <LevelAuthors :author="level.author" :creators="level.creators" :verifier="Array.isArray(level.verifier) ? level.verifier.join(', ') : level.verifier"></LevelAuthors>
-                    <div v-for="vid in video" :key="vid">
-                        <iframe :src="vid" frameborder="0" width="552" height="320" allowfullscreen></iframe>
-                    </div>
-                    <ul class="stats">
-                        <li>
-                            <div class="type-title-sm">Points when completed</div>
-                            <p>{{ score(selected + 1, 100, level.percentToQualify) }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">ID</div>
-                            <p>{{ level.id }}</p>
-                        </li>
-                        <li>
-                            <div class="type-title-sm">Password</div>
-                            <p>{{ level.password || 'Free to Copy' }}</p>
-                        </li>
-                    </ul>
-                </div>
-            </div>
         </main>
     `,
     data: () => ({
         list: [],
-        selectedPage: 'list',
+        editors: [],
         loading: true,
         selected: 0,
         errors: [],
@@ -113,20 +148,48 @@ export default {
             return this.list[this.selected][0];
         },
         video() {
-            return Array.isArray(this.level.verification)
-                ? this.level.verification.map(embed)
-                : [embed(this.level.verification)];
+            const verificationVideos = Array.isArray(this.level.verification)
+            ? this.level.verification.map(embed)
+            : [embed(this.level.verification)];
+                    
+            const showcaseVideos = Array.isArray(this.level.showcase)
+                ? this.level.showcase
+                : [this.level.showcase];
+    
+            const selectedVideos = this.toggledShowcase
+                ? showcaseVideos
+                : verificationVideos;
+    
+            return selectedVideos.map(embed); // Genera todos los videos
         },
     },
     async mounted() {
+        // Hide loading spinner
         this.list = await fetchList();
+        this.editors = await fetchLeaderboard();
+
+        // Error handling
+        if (!this.list) {
+            this.errors = [
+                "Failed to load list. Retry in a few minutes or notify list staff.",
+            ];
+        } else {
+            this.errors.push(
+                ...this.list
+                    .filter(([_, err]) => err)
+                    .map(([_, err]) => {
+                        return `Failed to load level. (${err}.json)`;
+                    })
+            );
+            if (!this.editors) {
+                this.errors.push("Failed to load list editors.");
+            }
+        }
+
         this.loading = false;
     },
     methods: {
         embed,
         score,
-        updateView() {
-            this.selected = 0;
-        }
     },
 };
